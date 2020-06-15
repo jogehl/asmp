95368b5df724a1206dd84cedc875c6111fccbd18
Joshua Gehlen <joshuagehlen@hotmail.com>
2020-06-15 18:54:27 +0200

    added comments to exercise 5

--- Ex5.py
+++ Ex5.py
@@ -7,10 +7,13 @@ from func import broadband_beamforming
 data_dict = {}
 fourier_channel_dict = {}
 
+# reead in sample rate
 sample_rate = wavfile.read("Data/Preprocessed_UAV_1.wav")[0]
 
+# number of bins for the fourier transform
 N = 400
 
+# read in the first second of all audio tracks and process the fourier transform
 for n in tqdm(range(0,16)):
     data_dict[n] = wavfile.read("Data/Preprocessed_UAV_" + str(n+1) + ".wav")[-1]
     data_dict[n] = data_dict[n][0:192000]
@@ -19,10 +22,11 @@ for n in tqdm(range(0,16)):
 
 fourier_channel_matrix = np.array(list(fourier_channel_dict.values()))
 
+# get only the first 200 bins because of symmetry
 freq_bins = np.fft.fftfreq(N, 1/sample_rate)[:200]
 
 
-
+# calculate the broadband_beamforming for uv coordinates with step size of 1/50
 steps = 50
 
 u_v_array = np.zeros((2 * steps, 2 * steps))
@@ -32,9 +36,10 @@ for u in tqdm(range(-steps,steps)):
         if (u/steps)**2 + (v/steps)**2 < 1:
             u_v_array[u+steps][v+steps] = broadband_beamforming(u/steps,v/steps,fourier_channel_matrix, freq_bins)
 
-extend = [-1,1,-1,1]
 
 u_v_array = np.flip(u_v_array,axis = 1)
+
+extend = [-1,1,-1,1]
 
 plt.clf()
 im = plt.imshow(u_v_array.T,extent = extend,cmap = "hsv")
